49c9a37f939f141f409f0af2ba06d1ea912720bb
crisandryj <crisandryj@gmail.com>
2023-01-16 11:48:01 -0500

    relationships notes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+ #has_many :name
+ #class_name: "Name"
+ #foreign_key: :name
+ #has_many: "name", through: :name
 
-#user has many friends as a follower
-#user has man friends as a followee
+  #user has many friendships as a follower
+  #user has man friendships as a followee
 
-has_many :follower_friends, class_name: "Friendship", foreign_key: :followee_id
-has_many :followers, through: :follower_friends
+has_many :friend_followers, class_name: "Friendship", foreign_key: :followee
+has_many :followers, through: :friend_followers
 
-has_many :followee_friends, class_name: "Friendship", foreign_key: :follower_id
-has_many :followees, through: :followee_friends
+has_many :friend_followees, class_name: "Friendship", foreign_key: :follower
+has_many :followees, through: :friend_followees
+
 
 
 
