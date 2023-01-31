55b05f85b2319a04e967a1ce866d4b9b3a330f24
crisandryj <crisandryj@gmail.com>
2023-01-31 09:48:18 -0500

    validates user only can like post once

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,9 @@
 class Like < ApplicationRecord
-  
-belongs_to :likeable, polymorphic: true
-belongs_to :user
+  validates :user_id, uniqueness: { scope: :likeable_id}
+
+
+  belongs_to :likeable, polymorphic: true
+  belongs_to :user
+
+
 end
