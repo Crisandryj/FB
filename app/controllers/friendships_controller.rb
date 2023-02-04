6d2fde85197dfc01869a6c7e7dbb3b01d077f4b6
crisandryj <crisandryj@gmail.com>
2023-02-04 10:47:18 -0500

    ability to accecpt friend added:

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,6 +4,7 @@ class FriendshipsController < ApplicationController
   end
 
   def create
+    raise params.inspect
     @user = current_user
     @friendship = Friendship.create(friendship_params)
     if @friendship.save
@@ -20,11 +21,26 @@ class FriendshipsController < ApplicationController
   end
 
   def edit
+    @friendship = Friendship.find(params[:id])
+  end
+
+  def update
+    @friendship = Friendship.find(params[:id])
+    @friendship.update(params_update)
+    if @friendship.save
+      redirect_to @friendship
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def friendship_params
     params.require(:friendship).permit(:followee_id,:follower_id, :accepted, :requestee_id, :requestor_id)
+  end
+
+  def params_update
+    params.permit(:accepted)
   end
 end
