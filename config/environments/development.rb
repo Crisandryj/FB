bda3944a0c28ada357843556ec25367e79913b16
crisandryj <crisandryj@gmail.com>
2023-02-12 08:40:53 -0500

    welcome email generator

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,6 @@ Rails.application.configure do
   #action_mailer
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
-  
   #devise configuration
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Settings specified here will take precedence over those in config/application.rb.
